52de768312562f9c7ae2390ace818e4c0d0e6609
Nick Hand <nhand@berkeley.edu>
2016-11-24 23:37:27 -0800

    Revert "using nthreads"
    This reverts commit 210bef4692897d13b27608d39de06632d50eb72d.

--- nbodykit/core/algorithms/WindowMultipoles.py
+++ nbodykit/core/algorithms/WindowMultipoles.py
@@ -2,7 +2,6 @@ from nbodykit.core import Algorithm, DataSource
 import numpy
 import os
 import logging
-from sharedmem import cpu_count
 
 logger = logging.getLogger('windowpaircount')
 
@@ -59,54 +58,52 @@ def paircount(datasource, poles, redges, comm=None, subsample=1):
     if comm.rank == 0: logger.info("BoxSize = %s" %str(BoxSize))
     
     # determine processor division for domain decomposition
-    #Nproc = [1, 1, 1]
-    #idx = numpy.argmax(BoxSize)
-    #Nproc[idx] = comm.size
+    Nproc = [1, 1, 1]
+    idx = numpy.argmax(BoxSize)
+    Nproc[idx] = comm.size
     
-    #if comm.rank == 0: logger.info('Nproc = %s' %str(Nproc))
+    if comm.rank == 0: logger.info('Nproc = %s' %str(Nproc))
     
-    #pos1 = pos1[comm.rank * subsample // comm.size ::subsample]
-    #ang_coords1 = ang_coords1[comm.rank * subsample // comm.size ::subsample]
-    #N1   = comm.allreduce(len(ang_coords1))
+    pos1 = pos1[comm.rank * subsample // comm.size ::subsample]
+    ang_coords1 = ang_coords1[comm.rank * subsample // comm.size ::subsample]
+    N1   = comm.allreduce(len(ang_coords1))
     
     # read position for field #2
-    #pos2 = pos1
-    #ang_coords2 = ang_coords1
-    #N2 = N1
-     
-    logger.info("cpu count = %d" %cpu_count()) 
-       
-    # # domain decomposition
-    # grid = [numpy.linspace(0, BoxSize[i], Nproc[i]+1, endpoint=True) for i in range(3)]
-    # domain = GridND(grid, comm=comm)
-    #
-    # # exchange field #1 positions
-    # layout = domain.decompose(pos1, smoothing=0)
-    # pos1 = layout.exchange(pos1)
-    # if comm.rank == 0: logger.info('exchange pos1')
-    #
-    # # exchange field #2 positions
-    # if Rmax > BoxSize.min() * 0.25:
-    #     ang_coords2 = numpy.concatenate(comm.allgather(ang_coords2), axis=0)
-    # else:
-    #     layout = domain.decompose(pos2, smoothing=Rmax)
-    #     ang_coords2 = layout.exchange(ang_coords2)
-    # N2   = comm.allreduce(len(ang_coords2))
-    # if comm.rank == 0: logger.info('exchange pos2')
+    pos2 = pos1
+    ang_coords2 = ang_coords1
+    N2 = N1
+        
+    # domain decomposition
+    grid = [numpy.linspace(0, BoxSize[i], Nproc[i]+1, endpoint=True) for i in range(3)]
+    domain = GridND(grid, comm=comm)
+    
+    # exchange field #1 positions
+    layout = domain.decompose(pos1, smoothing=0)
+    pos1 = layout.exchange(pos1)
+    if comm.rank == 0: logger.info('exchange pos1')
+
+    # exchange field #2 positions
+    if Rmax > BoxSize.min() * 0.25:
+        ang_coords2 = numpy.concatenate(comm.allgather(ang_coords2), axis=0)
+    else:
+        layout = domain.decompose(pos2, smoothing=Rmax)
+        ang_coords2 = layout.exchange(ang_coords2)
+    N2   = comm.allreduce(len(ang_coords2))
+    if comm.rank == 0: logger.info('exchange pos2')
 
     # log the sizes of the trees
-    #logger.info('rank %d correlating %d x %d' %(comm.rank, len(ang_coords1), len(ang_coords2)))
-    #if comm.rank == 0: logger.info('all correlating %d x %d' %(N1, N2))
+    logger.info('rank %d correlating %d x %d' %(comm.rank, len(ang_coords1), len(ang_coords2)))
+    if comm.rank == 0: logger.info('all correlating %d x %d' %(N1, N2))
 
     # do the pair counting
     kws                     = {}
     kws['is_comoving_dist'] = True
     kws['output_rpavg']     = True
-    #kws['RA2']              = ang_coords2[:,0]
-    #kws['DEC2']             = ang_coords2[:,1]
-    #kws['CZ2']              = ang_coords2[:,2]
+    kws['RA2']              = ang_coords2[:,0]
+    kws['DEC2']             = ang_coords2[:,1]
+    kws['CZ2']              = ang_coords2[:,2]
     kws['verbose']          = True
-    results = Corrfunc.mocks.DDrppi_mocks(1, 1, cpu_count(), Rmax, redges, ang_coords1[:,0], ang_coords1[:,1], ang_coords1[:,2], **kws)
+    results = Corrfunc.mocks.DDrppi_mocks(0, 1, 1, Rmax, redges, ang_coords1[:,0], ang_coords1[:,1], ang_coords1[:,2], **kws)
     logger.info('...rank %d done correlating' %(comm.rank))
     
     # do the sum across ranks
